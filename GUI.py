d572af0a098a86040f3fb4f0b46140994779d2f2
landonleighredline <lleigh@redlineforensic.studio>
2021-05-07 12:21:32 -0400

    Additional Functionality and Executable file

--- GUI.py
+++ GUI.py
@@ -1,17 +1,23 @@
 import tkinter as tk
 from tkinter import *
 import os
+from datetime import date
 
 #create GUI
 gui = tk.Tk()
 gui.geometry('600x300')
+gui.configure(bg='#292d34')
+
+#Get todays date
+today = date.today()
+date = today.strftime("%m-%d-%y")
 
 #Array of options for location dropdown
 locationOptions = [
-    'Atlanta',
-    'Denver',
-    'Nashville',
-    'Charlotte'
+    'ATL',
+    'DEN',
+    'NAS',
+    'CLT'
 ]
 
 #Array of options for program dropdown
@@ -29,14 +35,14 @@ def retrieveJobInput():
 def retrieveVehicleInput():
     vehicleInput = self.vehicleBox.get("1.0",END)
 
-#Function to create text file for jobs
-def createFile():
-    f = open('myfile.txt', 'w')
 
-#Function to add jobs to existing file
+#Function to add jobs to text file
 def addFile():
-    f = open('myfile.txt', 'a')
-    f.write()
+    filename = date + '.txt'
+    #f = open(filename, 'w')
+    #f.close()
+    f = open(filename, 'a+')
+    f.write('info')
     f.close()
 
 #Function to start the script
@@ -45,48 +51,44 @@ def startScript():
 
 #Job Number Text Box
 jobLabel = Label(gui, text='Job Number')
-jobLabel = Label(gui, text='Job Number')
-jobBox = Text(gui, bg='white', height=1, width=10, padx=5, pady=5)
+jobLabel.configure(bg='#302c34', fg='white')
 jobBox = Text(gui, bg='white', height=1, width=10, padx=5, pady=5)
 jobLabel.pack()
 jobBox.pack()
 
 #Job Location Dropdown
 locationLabel = Label(gui, text='Job Location')
-locationLabel = Label(gui, text='Job Location')
+locationLabel.configure(bg='#302c34', fg='white')
 locationPlaceholder = StringVar()
-locationPlaceholder.set('Atlanta')
-locaionDrop = OptionMenu(gui, locationPlaceholder,  *locationOptions)
-locaionDrop = OptionMenu(gui, locationPlaceholder,  *locationOptions)
+locationPlaceholder.set('ATL')
+locationDrop = OptionMenu(gui, locationPlaceholder,  *locationOptions)
+locationDrop.configure(bg='#889099')
 locationLabel.pack()
-locaionDrop.pack()
+locationDrop.pack()
 
 #Vehicle Text Box
 assetLabel = Label(gui, text='Asset Name')
-assetLabel = Label(gui, text='Asset Name')
-assetBox = Text(gui, bg='white', height=1, width=10, padx=5, pady=5)
+assetLabel.configure(bg='#302c34', fg='white')
 assetBox = Text(gui, bg='white', height=1, width=10, padx=5, pady=5)
 assetLabel.pack()
 assetBox.pack()
 
 #Program Dropdown
 programLabel = Label(gui, text='Program Used')
+programLabel.configure(bg='#302c34', fg='white')
 programPlaceholder = StringVar()
 programPlaceholder.set('Scene')
 programDrop = OptionMenu(gui, programPlaceholder,  *programOptions)
+programDrop.configure(bg='#889099')
 programLabel.pack()
 programDrop.pack()
 
-#Create File Button
-createFileButton = tk.Button(gui, text='Create File', padx=10, pady=5, fg='white', bg='#465061', command=createFile)
-createFileButton.pack()
-
 #Add to File Button
-addFileButton = tk.Button(gui, text='Add to File', padx=10, pady=5, fg='white', bg='#465061', command=addFile)
+addFileButton = tk.Button(gui, text='Add Job', padx=10, pady=5, fg='black', bg='#889099', command=addFile)
 addFileButton.pack()
 
 #Start Script Button
-startScriptButton = tk.Button(gui, text='Start Script', padx=10, pady=5, fg='white', bg='#465061', command=startScript)
+startScriptButton = tk.Button(gui, text='Start Script', padx=10, pady=5, fg='black', bg='#889099', command=startScript)
 startScriptButton.pack()
 
 gui.mainloop()
